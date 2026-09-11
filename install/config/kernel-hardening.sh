c14cc4e529b0891d1a73ad3224b72885d70cf80b
Alessio Attilio <attilio.alessio@protonmail.com>
2026-09-11 14:03:55 +0200

    Kernel sysctl hardening: ASLR, ptrace, dmesg, BPF, network, filesystem

---
+++ install/config/kernel-hardening.sh
@@ -0,0 +1,57 @@
+# Kernel security hardening via sysctl
+sudo tee /etc/sysctl.d/99-omarchy-security.conf >/dev/null <<'EOF'
+kernel.randomize_va_space = 2
+kernel.kptr_restrict = 2
+kernel.dmesg_restrict = 1
+kernel.perf_event_paranoid = 3
+kernel.unprivileged_bpf_disabled = 1
+kernel.yama.ptrace_scope = 1
+kernel.core_uses_pid = 1
+kernel.sysrq = 0
+kernel.io_uring_disabled = 2
+fs.suid_dumpable = 0
+fs.protected_hardlinks = 1
+fs.protected_symlinks = 1
+fs.protected_fifos = 2
+fs.protected_regular = 2
+fs.file-max = 2000000
+vm.mmap_rnd_bits = 32
+vm.mmap_rnd_compat_bits = 16
+net.ipv4.conf.all.rp_filter = 1
+net.ipv4.conf.default.rp_filter = 1
+net.ipv4.conf.all.accept_redirects = 0
+net.ipv4.conf.default.accept_redirects = 0
+net.ipv4.conf.all.secure_redirects = 0
+net.ipv4.conf.default.secure_redirects = 0
+net.ipv4.conf.all.send_redirects = 0
+net.ipv4.conf.default.send_redirects = 0
+net.ipv4.conf.all.accept_source_route = 0
+net.ipv4.conf.default.accept_source_route = 0
+net.ipv4.conf.all.log_martians = 1
+net.ipv4.conf.default.log_martians = 1
+net.ipv4.icmp_echo_ignore_broadcasts = 1
+net.ipv4.icmp_ignore_bogus_error_responses = 1
+net.ipv4.tcp_syncookies = 1
+net.ipv4.tcp_max_syn_backlog = 4096
+net.ipv4.tcp_synack_retries = 2
+net.ipv4.tcp_syn_retries = 5
+net.ipv4.tcp_rfc1337 = 1
+net.ipv4.tcp_timestamps = 0
+net.ipv4.ip_forward = 0
+net.ipv4.conf.all.mc_forwarding = 0
+net.ipv4.conf.default.mc_forwarding = 0
+net.core.bpf_jit_enable = 0
+net.core.bpf_jit_harden = 2
+net.ipv6.conf.all.accept_redirects = 0
+net.ipv6.conf.default.accept_redirects = 0
+net.ipv6.conf.all.accept_source_route = 0
+net.ipv6.conf.default.accept_source_route = 0
+net.ipv6.conf.all.accept_ra = 0
+net.ipv6.conf.default.accept_ra = 0
+net.ipv6.conf.all.disable_ipv6 = 0
+net.ipv6.conf.default.disable_ipv6 = 0
+net.ipv6.conf.all.mc_forwarding = 0
+net.ipv6.conf.default.mc_forwarding = 0
+EOF
+
+sudo sysctl --system >/dev/null 2>&1
