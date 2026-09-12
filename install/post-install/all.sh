b1b284d985260ea0e733ae6647d8e4f82ac1cc91
Taksh <takshkothari09@gmail.com>
2026-09-12 22:16:52 +0530

    Make /boot not world-accessible

--- install/post-install/all.sh
+++ install/post-install/all.sh
@@ -1,3 +1,5 @@
 run_logged "$OMARCHY_INSTALL/post-install/pacman.sh"
 run_logged "$OMARCHY_INSTALL/post-install/udev.sh"
 run_logged "$OMARCHY_INSTALL/post-install/localdb.sh"
+
+run_logged "$OMARCHY_INSTALL/post-install/boot-permissions.sh"
