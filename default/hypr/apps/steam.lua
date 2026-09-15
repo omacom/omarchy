e3a6eaeb85445cf8d94c30a1f8c2971870588105
flrsn <40324093+flrsn@users.noreply.github.com>
2026-09-15 19:44:27 +0200

    Fix fullscreen Steam game window rules

--- default/hypr/apps/steam.lua
+++ default/hypr/apps/steam.lua
@@ -1,4 +1,4 @@
-o.window("steam", { float = true, idle_inhibit = "fullscreen" })
-o.window({ class = "steam", title = "Steam" }, { center = true, size = { 1100, 700 } })
-o.window("steam.*", { tag = "-default-opacity", opacity = "1 1" })
-o.window({ class = "steam", title = "Friends List" }, { size = { 460, 800 } })
+o.window({ class = "steam", title = "Steam" }, { float = true, center = true, size = { 1100, 700 } })
+o.window("steam.*", { tag = "-default-opacity", opacity = "1 1", idle_inhibit = "fullscreen" })
+o.window("gamescope", { idle_inhibit = "fullscreen" })
+o.window({ class = "steam", title = "Friends List" }, { float = true, size = { 460, 800 } })
